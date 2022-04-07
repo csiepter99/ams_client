c5f1c5e07ac85a2a08e65fcbe28aa5e8b2de0f31
peter99 <csiepter99@gmail.com>
2022-04-07 23:16:55 +0800

    Init UI design.
    (cherry picked from commit 0384e86d0ca0c2a095c5cf9c28575b2b5691807f)

---
+++ src/components/HomePage.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      temporary
+    >
+      <v-list
+        nav  
+      >
+        <v-list-item-group
+          v-model="group"
+        >
+          <v-list-item
+            v-for="(item, i) in menuItems"
+            :key="i"
+            link
+            :to="item.route"
+          >
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Asset Manager System</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>mdi-qrcode-scan</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        menuItems: [
+            { title: "財產清單", route: "/assets" },
+            { title: "待購清單", route: "/toBuy" }
+        ],
+        drawer: null,
+        group: null,
+    }),
+    watch: {
+        group() {
+            this.drawer = false;
+        },
+    },
+}
+</script>
+
+<style>
+</style>

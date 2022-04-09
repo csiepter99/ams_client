8cca0954cd3d84ff755ca3ff36e3c67b9675fc53
110598082 <110598082@ntut.org.tw>
2022-04-09 21:51:17 +0800

    Connect to server side.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,17 @@
 <script>
 // @ is an alias to /src
 import HomePage from '@/components/HomePage.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     HomePage
+  },
+  mounted() {
+    axios
+      .get('http://localhost:8088/api/asset')
+      .then(res => console.log(res));
   }
 }
 </script>
